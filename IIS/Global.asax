b7847266e0a16df9c1df049c4f15b8030500cdfe
834930269 <834930269@qq.com>
2017-12-26 07:53:37 +0800

    all

---
+++ IIS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CW_Blog.MvcApplication" Language="C#" %>
---
+++ IIS/Scripts/ScroolBack.js
@@ -0,0 +1,31 @@
+﻿function ScrollBack() {
+    var obtn = document.getElementById('scrollback');
+    var height = document.documentElement.clientHeight;//鑾峰彇鍙鍖哄煙楂樺害
+    var timer = null;
+    window.onscroll = function () {
+        var a = true;
+        var osTop = document.documentElement.scrollTop || document.body.scrollTop;//鑾峰彇婊氬姩鏉＄殑浣嶇疆锛岃繑鍥炴暟鍊�,,璋锋瓕鏀寔鍚庨潰鐨�
+        if (osTop >= (0)) {
+            obtn.style.display = "block";
+        } else {
+            obtn.style.display = "none";
+        }
+        if (!a) {
+            clearInterval(timer);
+        }
+    }
+    obtn.onclick = function () {
+        //璁剧疆瀹氭椂鍣�
+        timer = setInterval(function () {
+            var osTop = document.documentElement.scrollTop || document.body.scrollTop;//鑾峰彇婊氬姩鏉＄殑浣嶇疆锛岃繑鍥炴暟鍊�,,璋锋瓕鏀寔鍚庨潰鐨�
+            var speed = Math.floor(-osTop / 6);
+
+            document.documentElement.scrollTop = document.body.scrollTop = osTop + speed;
+
+            if (osTop == 0) {
+                clearInterval(timer);
+            }
+        }, 30)//鍔ㄧ敾锛屽畾鏃跺櫒	
+    }
+}
+ScrollBack();
